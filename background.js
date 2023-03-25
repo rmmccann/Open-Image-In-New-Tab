adaf4d4d5470384c9f7202380fd8f2fec39ffef2
Ryan McCann <ryan@ryan-mccann.com>
2023-03-24 20:24:10 -0400

    fixed incorrect mime type for some images

--- background.js
+++ background.js
@@ -9,9 +9,10 @@ browser.menus.create({
 async function openImageInNewTab(info)
 {
 	var imageUrl = info.srcUrl;
+	var parsedUrl = new URL(imageUrl);
 
 	// embedded images
-	if(imageUrl.startsWith("imap://") || imageUrl.startsWith("pop://") || imageUrl.startsWith("mailbox://"))
+	if(["imap:", "pop:", "mailbox:"].includes(parsedUrl.protocol))
 	{
 		var currentTab = (await browser.tabs.query({active: true, currentWindow: true}))[0];
 		var currentMessage = await browser.messageDisplay.getDisplayedMessage(currentTab.id);
@@ -20,13 +21,13 @@ async function openImageInNewTab(info)
 		var parser = new postalMime.default();
 		var parsedEmail = await parser.parse(currentMessageRaw);
 
-		var parsedUrl = new URL(imageUrl);
 		var filename = parsedUrl.searchParams.get("filename");
+		var mimeType = parsedUrl.searchParams.get("type");
 
 		parsedEmail.attachments.forEach(function(attachment) {
 			if(attachment.filename === filename)
 			{
-				imageUrl = URL.createObjectURL(new Blob([attachment.content], {type: attachment.mimeType}));
+				imageUrl = URL.createObjectURL(new Blob([attachment.content], {type: mimeType}));
 			}
 		});
 	}
